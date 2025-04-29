1c9a24d0db01d0eddd7f3359f4681551f1b57098
zkrakus <zkrakus@gmail.com>
2025-04-29 12:27:18 -0400

    feat:s06-l107 Added css component selector for icons.

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+  
 }
